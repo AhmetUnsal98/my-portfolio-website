1243e1c6d78d63e4cb246c06e206627c44ab3620
[Ahmet Ünsal] <[unsalahmet1998@outlook.com]>
2023-03-24 18:58:20 +0300

    project links added

--- src/components/Works/Components/Testimonial.jsx
+++ src/components/Works/Components/Testimonial.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./testimonial.scss";
-import { IoLogoGithub } from "react-icons/io5";
+import {
+  IoLogoGithub,
+  IoSearchCircle,
+  IoSearchOutline,
+  IoServerOutline,
+} from "react-icons/io5";
 import { motion, AnimatePresence } from "framer-motion";
 import resume from "../../../assets/proje1.png";
 import ecommerce from "../../../assets/ecommerce.png";
@@ -10,6 +15,7 @@ const testimonials = [
     id: "1",
     title: "Clothing E-Commerce Shop",
     link: "https://github.com/AhmetUnsal98/mern-stack-ecommerce",
+    project: "https://ecommerce-ahmet-clothing.netlify.app/",
     img: ecommerce,
     desc: "Fullstack e-commerce application with payment system a fully completed project is ready for use",
     tags: [
@@ -27,6 +33,7 @@ const testimonials = [
     id: "2",
     title: "Resume Creator",
     link: "https://github.com/AhmetUnsal98/make-your-cv",
+    project: "https://your-resume-creator.netlify.app/",
     img: resume,
     desc: "This is an application for creating resume. It has progress to creating resume.",
     tags: ["html", "css", "scss", "react", "redux", "typescript"],
@@ -35,6 +42,7 @@ const testimonials = [
     id: "3",
     title: "Getir Clone",
     link: "https://github.com/AhmetUnsal98/getirclonescss",
+    project: "https://getir-clone-ahmet.netlify.app",
     img: getir,
     desc: "Getir clone with pixel perfect",
     tags: ["html", "css", "scss", "react"],
@@ -84,6 +92,12 @@ const Testimonial = ({ index, direction }) => {
           <div className="github-icon">
             <a href={testimonials[index].link}>
               <IoLogoGithub color="white" size={25} />
+            </a>
+          </div>
+
+          <div className="search-icon">
+            <a href={testimonials[index].project}>
+              <IoSearchOutline color="white" size={25} />
             </a>
           </div>
         </motion.div>
